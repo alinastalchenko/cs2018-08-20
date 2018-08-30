92b9bef9c99f3f3f76ad8e416ef669043b8abda0
alinastalchenko <alinastalchenko@mail.ru>
2018-08-31 02:38:37 +0300

    lesson 5

--- src/by/it/alinastalchenko/lesson05/TaskC2.java
+++ src/by/it/alinastalchenko/lesson05/TaskC2.java
@@ -1,6 +1,7 @@
 package by.it.alinastalchenko.lesson05;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
@@ -9,7 +10,7 @@ import java.util.Collections;
 Задача: Написать программу, которая вводит с клавиатуры 20 чисел и выводит их в убывающем порядке.
 */
 public class TaskC2 {
-public static void main(String[]args){
+public static void main(String[]args) throws IOException {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Integer[] array = new Integer[20];
         for (int i = 0; i < 20; i++)
